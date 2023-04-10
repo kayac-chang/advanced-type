5e5ac927e4c050b3cc123e7ce0e801d0396117c5
kayac-chang <egg734631@gmail.com>
2023-04-10 23:12:04 +0800

    feat: 🎸 prepend

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 - HasTail: `HasTail` takes an array and returns true if it has more than one element.
 - Last: `Last` takes an array and returns its last element.
 - Length: `Length` takes an array and returns its length.
+- Prepend: `Prepend` takes an element and an array and returns a new array with the element prepended.
---
+++ src/Prepend.ts
@@ -0,0 +1,17 @@
+import { Equal } from "./Equal";
+import { Expect } from "./Expect";
+
+/**
+ * `Prepend` takes an element and an array and returns a new array with the element prepended.
+ */
+type Prepend<E, T extends any[]> = [E, ...T];
+
+export type Test1 = Expect<Equal<Prepend<1, [2, 3]>, [1, 2, 3]>>;
+export type Test2 = Expect<Equal<Prepend<1, []>, [1]>>;
+export type Test3 = Expect<Equal<Prepend<1, [2]>, [1, 2]>>;
+export type Test4 = Expect<
+  Equal<
+    Prepend<1, [2, 3, 4, 5, 6, 7, 8, 9, 10]>,
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  >
+>;
